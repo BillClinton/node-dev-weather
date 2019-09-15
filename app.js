5866ccff45b10decf51024a85642b1b9ca63090e
BillClinton <bclinton@appunto.net>
2019-09-15 01:49:12 -0400

    Accept location as command line argument

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
+const yargs = require('yargs');
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-geocode('Philadelphia', (error, geoData) => {
-  if (error) {
-    return console.log(error);
-  }
-  forecast(geoData.long, geoData.lat, (error, forecastData) => {
-    if (error) {
-      return console.log(error);
+// Customize yargs version
+yargs.version('1.0.0');
+
+yargs.command({
+  command: 'forecast',
+  describe: "Get today's weater forecast from darksky.net",
+  builder: {
+    location: {
+      describe: 'Location',
+      demandOption: true,
+      type: 'string'
     }
-    console.log(geoData.location);
-    console.log(forecastData.msg);
-  });
+  },
+  handler(argv) {
+    geocode(argv.location, (error, geoData) => {
+      if (error) {
+        return console.log(error);
+      }
+      forecast(geoData.long, geoData.lat, (error, forecastData) => {
+        if (error) {
+          return console.log(error);
+        }
+        console.log(geoData.location);
+        console.log(forecastData.msg);
+      });
+    });
+  }
 });
+
+yargs.parse();
