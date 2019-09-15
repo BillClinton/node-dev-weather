5b7a1ed7648b6b57f3cb7c077d4115a6bc99c6df
BillClinton <bclinton@appunto.net>
2019-09-14 22:10:43 -0400

    Add error handling and keys from config file

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
 const request = require('request');
 
-const mapApiKey =
-  'pk.eyJ1IjoiYmNsaW50b24iLCJhIjoiY2swazhpbjc0MGowOTNibzI2d25wMG4wZyJ9.CL4r9LkECXKFzEyJ2FkM_A';
-const mapUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${mapApiKey}`;
+const config = require('./config');
+
+const placeName = 'Philadelphia,Pa';
+//const placeName = 'asdsadsa';
+
+const mapApiKey = config.mapApiKey;
+//'pk.eyJ1IjoiYmNsaW50b24iLCJhIjoiY2swazhpbjc0MGowOTNibzI2d25wMG4wZyJ9.CL4r9LkECXKFzEyJ2FkM_A';
+const mapUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${placeName}.json?access_token=${mapApiKey}`;
 
 request({ url: mapUrl, json: true }, (error, response) => {
-  console.log(response.body.features[0].center[0]);
-  console.log(response.body.features[0].center[1]);
-});
+  if (error) {
+    console.log('unable to connect to map service');
+  } else if (response.body.features.length <= 0) {
+    console.log('Map service returned no data. Unable to find location.');
+  } else {
+    const long = response.body.features[0].center[0];
+    const lat = response.body.features[0].center[1];
 
-const weatherUrl =
-  'https://api.darksky.net/forecast/14020ff4a5050ff1a644feb71cde65d7/40.122266,-75.083700';
+    console.log(long);
+    console.log(lat);
 
-request({ url: weatherUrl, json: true }, (error, response) => {
-  const c = response.body.currently;
-  const d = response.body.daily;
-  const msg = `${d.data[0].summary}  It is currently ${c.temperature} degrees. There is a ${c.precipProbability}% chance of rain.`;
+    const weatherApiKey = config.weatherApiKey;
+    const weatherUrl = `https://api.darksky.net/forecast/${weatherApiKey}/${long},${lat}`;
 
-  console.log(msg);
+    request({ url: weatherUrl, json: true }, (error, response) => {
+      if (error) {
+        console.log('unable to connect to weather service');
+      } else if (response.body.error) {
+        console.log('Unable to retieve weather information');
+      } else {
+        const c = response.body.currently;
+        const d = response.body.daily;
+        const msg = `${d.data[0].summary}  It is currently ${c.temperature} degrees. There is a ${c.precipProbability}% chance of rain.`;
+
+        console.log(msg);
+      }
+    });
+  }
 });
